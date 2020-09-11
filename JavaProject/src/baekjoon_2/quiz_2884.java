81f0350d353d883b8f050770c18bd149c2fab0a1
dohee08 <53321232+dohee08@users.noreply.github.com>
2020-09-11 21:36:23 +0900

    if문, for문

--- JavaProject/src/baekjoon_2/quiz_2884.java
+++ JavaProject/src/baekjoon_2/quiz_2884.java
@@ -6,11 +6,20 @@ public class quiz_2884 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		int hour = scan.nextInt();
+		int min = scan.nextInt();
 		
+		if(min < 45) {
+			hour = hour-1;
+			min = 60-(45-min);
+			if(hour<0) {
+				hour = 23;
+			}
+			System.out.println(hour+" "+min);
+		}else {
+			System.out.println(hour+" "+(min-45));
+		}
 		
-
 	}
 
 }
