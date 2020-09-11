7879f1bc8aa1b304d4bd509e6e4454d516d513a5
dohee08 <53321232+dohee08@users.noreply.github.com>
2020-09-11 22:12:01 +0900

    4-2) 입력이 끝날 때까지 A+B를 출력하는 문제. EOF에 대해 알아 보세요.

---
+++ JavaProject/src/baekjoon_4_while문/quiz_10951.java
@@ -0,0 +1,19 @@
+package baekjoon_4_while¹®;
+
+import java.util.Scanner;
+
+public class quiz_10951 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextInt()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			System.out.println(a+b);
+		}
+
+	}
+
+}
