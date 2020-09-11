186804d31346ed9b3edd68f05dadae48a0904764
dohee08 <53321232+dohee08@users.noreply.github.com>
2020-09-11 23:25:11 +0900

    4-3) 원래 수로 돌아올 때까지 연산을 반복하는 문제

---
+++ JavaProject/src/baekjoon_4_while문/quiz_1110.java
@@ -0,0 +1,33 @@
+package baekjoon_4_while문;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class quiz_1110 {
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+
+		int num = scan.nextInt(); //원래 수
+
+		int count = 0;
+		int result = num;
+		
+		while(true) {
+			int num1 = num/10; //10의 자리
+			int num2 = num%10; //1의 자리
+			int sum = num1+num2;
+			
+			num = (num2*10)+(sum%10);
+			count++;
+			
+			if(result == num) {
+				break;
+			}
+			
+		}
+		System.out.println(count);
+
+	}
+
+}
