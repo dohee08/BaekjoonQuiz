01cad730de022dfe5d50a0d2eae4bcf65626ab53
dohee08 <53321232+dohee08@users.noreply.github.com>
2020-10-05 15:44:08 +0900

    5-5) 평균을 조작하는 문제

---
+++ JavaProject/src/baekjoon_5_1차원배열/Quiz_1546.java
@@ -0,0 +1,40 @@
+package baekjoon_5_1차원배열;
+
+import java.util.Scanner;
+
+public class Quiz_1546 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double[] score = new double[scan.nextInt()];
+		
+		for(int i=0; i<score.length; i++) {
+			score[i] = scan.nextInt();
+		}
+		
+		//최대값 구하기
+		double max = 0;
+		int idx = 0;
+		for(int k=0; k<score.length; k++) {
+			if(score[k] > max) {
+				max = score[k];
+				idx = k;
+			}
+		}
+		
+		//위치 빼고 새로 계산
+		double[] temp = new double[score.length];
+		for(int j=0; j<score.length; j++) {
+			temp[j] = score[j]/max*100;
+			}
+		
+		//새로 계산한 값 평균
+		double sum = 0;
+		for(int i=0; i<temp.length; i++) {
+			sum = sum + temp[i];
+		}
+		
+		System.out.println(sum/score.length);
+	}
+
+}
